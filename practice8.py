3b82c80ac35a608f944d149e0a60832e7e9faec1
Farid <Farid.darabi@gmail.com>
2022-05-24 22:04:38 +0430

    refactor

--- practice8.py
+++ practice8.py
@@ -1,13 +1,23 @@
 from gevent import subprocess
 import json
 
-def runCommands(json_string):
+commands = ['os', 'compute']
+
+
+def run_command(json_string):
     a = json.loads(json_string)
     command_type = a['command_type']
+    if command_type not in commands:
+        raise Exception('command not found')
+
+    data = {}
     if command_type == 'os':
         command_name = a['command_name']
         parameters = a['parameters']
-        given_os_command = command_name + '/'.join(parameters)
+        given_os_command = command_name
+        if parameters:
+            given_os_command = given_os_command + ' ' + ' '.join(parameters)
+        print(given_os_command)
         with subprocess.Popen(
                 given_os_command,
                 stdout=subprocess.PIPE,
@@ -17,10 +27,9 @@ def runCommands(json_string):
 
             result = proc.stdout.read()
             data = {
-                    "given_os_command": given_os_command,
-                    "result": result
-                    }
-
+                "given_os_command": given_os_command,
+                "result": result
+            }
     elif command_type == 'compute':
         expression = a['expression']
 
@@ -29,17 +38,12 @@ def runCommands(json_string):
             "given_math_expression": expression,
             "result": result
         }
-        ...
+
     return data
-
-
-if __name__ == "__main__":
-    runCommands('{"command_type": "os", "command_name": "dir", "parameters": ["/A", "s"]}')
 
 # echo Hello from the other side!
 # '{"command_type": "os", "command_name": "dir", "parameters": ["/home/me", "-h", "-l"]}'
 # '{"command_type": "compute", "expression": "((30+10)*5+1+1)"}'
 
-
-
-
+# linux command:
+# {"command_type": "os", "command_name": "ls", "parameters": ["~", "-a"]}
